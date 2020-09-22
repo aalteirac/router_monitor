753bc6183817b27baabc50fc6f643982c43ea250
Anthony Alteirac <anthony@alteirac.com>
2020-09-22 12:45:24 +0200

    candiate

--- index.js
+++ index.js
@@ -2,34 +2,63 @@ var router = require('./lib/router').create({
     gateway: '192.168.8.1'
   });
 
-    router.getToken(function(error, token) {
-        router.login(token,"admin","anthony1",function(error,response){
-            console.log(response==="OK"?"LOGGED":"ERROR:"+error);
-            // router.getRadioSettings(token,function(error,response){
-            //     console.log(response);
-            // })
-            router.getToken(function(error, token) {
-                //networkMode, networkBand, lteBand,
-                router.setRadioSettings(token,"03","3FFFFFFF","4",function(error,response){
-                    console.log(response==="OK"?"OK":"ERROR:"+error);
-                }) 
-            },token)
-        });
-  });
+  async function go(){
+    var token=await router.getToken();
+    // console.log(token.token || token);
+    var ret=await router.login(token,"admin","anthony1");
+    // console.log(ret.response || ret.error);
+    // ret=await router.getNotifications(token);
+    // console.log(ret.response || ret.error);
+    // ret=await router.setRadioSettings(token,"00","3FFFFFFF","8000044");//networkMode, networkBand, lteBand
+    // console.log(ret.response || ret.error);
+    // ret=await router.getRadioSettings(token);
+    // console.log(ret.response || ret.error);
+    // ret=await router.getSMSList(1,token);
+    // console.log(ret);
+    // var toDel=[];
+    // ret.response.map(async (m)=>{
+    //   if(m.Smstat[0]=='0'){
+    //     console.log(m.Content[0] + " will be processed!");
+    //     toDel.push(m.Index[0])
+    //   }
+    // })
+    // if(toDel.length>0){
+    //   var ret=await router.deleteSMS(toDel,token);
+    //   console.log(ret);
+    // }
+    // ret=await router.requestReboot(token);
+    // console.log(ret.response || ret.error);
+  }
+
+  async function monitor(){
+    var tk=await router.getToken();
+    var ret=await router.login(tk,"admin","anthony1");
+    setInterval(async ()=>{
+      var ret=await router.ping(tk);
+      console.log(ret.response || ret.error);
+
+    },20000)
+  }
+  
+  monitor();
 
 
-// GET/api/net/net-mode 
-// 4G 700, 1800, 2600
-// <response>
-// <NetworkMode>0302</NetworkMode>
-// <NetworkBand>3FFFFFFF</NetworkBand>
-// <LTEBand>8000044</LTEBand>
-// </response>
+ 
+//networkMode=08=5G,03=4G,02=3G,01=2G,00=Auto,0803=4G+5G,0302=3G+4G
+//lteband(upload)=0.7=8000000,all=8000044,1.8=4,2.1=1,2.6=40,0.8=80000,0.9=80
 
-//POST/api/net/net-mode
-// <request>
-// <LTEBand>4</LTEBand>
-// <NetworkMode>0302</NetworkMode>
-// <NetworkBand>3FFFFFFF</NetworkBand>
-// </request>
+//my best setting, well... depends...
+// {
+//   NetworkMode: [ '00' ],
+//   NetworkBand: [ '3FFFFFFF' ],
+//   LTEBand: [ '8000044' ] or 4 
+// }
+
+// Réseau:	Free
+// MCC-MNC:	208-15
+// TAC:	1200
+// CellId:	106059337
+// eNB Id:	414294 (secteur 73)
+// UL:	3478 MHhz (band 22)
+// DL:	1538 MHhz (band 24)
 
